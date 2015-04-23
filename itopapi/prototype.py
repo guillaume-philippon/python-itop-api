e5d6053b8674b5cef73f108f6c6d975c5f9279b7
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-04-23 15:56:43 +0200

    Split URI into prototype

--- itopapi/prototype.py
+++ itopapi/prototype.py
@@ -18,6 +18,8 @@ class ItopapiPrototype(object):
         self.username = None;
         self.password = None;
         self.itop_name = None;
+        self.protocole = 'https'
+        self.base_suffix = '/itop'
 
     def _params_(self, json_data):
         return urllib.urlencode({
@@ -31,7 +33,7 @@ class ItopapiPrototype(object):
         json_data = json.dumps({
             'operation': 'list_operations'
         })
-        uri = 'https://{0}{1}'.format(self.hostname, self.api_suffix)
+        uri = '{0}://{1}{2}{3}'.format(self.protocole, self.hostname, self.base_suffix, self.api_suffix)
         params = self._params_(json_data)
         return json.loads(urllib2.urlopen(uri, params).read())
 
@@ -41,6 +43,6 @@ class ItopapiPrototype(object):
             'class': self.itop_name,
             'key': 'SELECT {0}'.format(self.itop_name),
         })
-        uri = 'https://{0}{1}'.format(self.hostname, self.api_suffix)
+        uri = '{0}://{1}{2}{3}'.format(self.protocole, self.hostname, self.base_suffix, self.api_suffix)
         params = self._params_(json_data)
         return json.loads(urllib2.urlopen(uri, params).read())
