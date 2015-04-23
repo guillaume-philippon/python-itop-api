424d04c0da9da5cc49cea3d34f509d00fdc5bc74
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-04-23 16:01:45 +0200

    Add _uri_ to construct uri

--- itopapi/prototype.py
+++ itopapi/prototype.py
@@ -18,8 +18,15 @@ class ItopapiPrototype(object):
         self.username = None
         self.password = None
         self.itop_name = None
-        self.protocole = 'https'
+        self.protocol = 'https'
         self.base_suffix = '/itop'
+
+    def _uri_(self):
+        return '{0}://{1}{2}{3}'.format(self.protocol,
+                                 self.hostname,
+                                 self.base_suffix,
+                                 self.api_suffix)
+
 
     def _params_(self, json_data):
         return urllib.urlencode({
@@ -33,7 +40,7 @@ class ItopapiPrototype(object):
         json_data = json.dumps({
             'operation': 'list_operations'
         })
-        uri = '{0}://{1}{2}{3}'.format(self.protocole, self.hostname, self.base_suffix, self.api_suffix)
+        uri = self._uri_()
         params = self._params_(json_data)
         return json.loads(urllib2.urlopen(uri, params).read())
 
@@ -43,6 +50,6 @@ class ItopapiPrototype(object):
             'class': self.itop_name,
             'key': 'SELECT {0}'.format(self.itop_name),
         })
-        uri = '{0}://{1}{2}{3}'.format(self.protocole, self.hostname, self.base_suffix, self.api_suffix)
+        uri = self._uri_()
         params = self._params_(json_data)
         return json.loads(urllib2.urlopen(uri, params).read())
