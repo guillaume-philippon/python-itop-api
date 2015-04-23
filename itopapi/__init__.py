f9649d32eb27a44501e4dda8c8f063df3d53ee26
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-04-23 16:54:11 +0200

    Some fixes to catch exception if no objects

--- itopapi/__init__.py
+++ itopapi/__init__.py
@@ -6,3 +6,4 @@ __version__ = '1.0'
 __authors__ = ['Guillaume Philippon <guillaume.philippon@lal.in2p3.fr>']
 
 from itopapi.rack import ItopapiRack
+from itopapi.server import ItopapiServer
--- itopapi/server.py
+++ itopapi/server.py
@@ -8,7 +8,7 @@ __version__ = '1.0'
 __authors__ = ['Guillaume Philippon <guillaume.philippon@lal.in2p3.fr>']
 
 
-class ItopapiServers(ItopapiPrototype):
+class ItopapiServer(ItopapiPrototype):
     """
     ItopapiServers is a object that represent a Servers from iTop
     """
@@ -16,5 +16,5 @@ class ItopapiServers(ItopapiPrototype):
         """
         Add itop['name'] to use generic ItopapiPrototype method
         """
-        super(ItopapiServers, self).__init__()
+        super(ItopapiServer, self).__init__()
         self.itop['name'] = 'Server'
