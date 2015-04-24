8dfee7fff515a51116a4893e1a67870c652e2d7c
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-04-24 14:26:49 +0200

    Move Config class definition into itopapi instead of model

--- itopapi/__init__.py
+++ itopapi/__init__.py
@@ -8,3 +8,4 @@ __version__ = '1.0'
 __authors__ = ['Guillaume Philippon <guillaume.philippon@lal.in2p3.fr>']
 
 from itopapi.model import ItopapiRack, ItopapiServer
+from itopapi.config import Config
