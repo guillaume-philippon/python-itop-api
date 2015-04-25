b36112af19ffe0e3dae93907795607ebc81fe3f1
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-04-25 10:11:54 +0200

    Rename Config to ItopapiConfig to avoid name conflict

--- itopapi/__init__.py
+++ itopapi/__init__.py
@@ -8,4 +8,4 @@ __version__ = '1.0'
 __authors__ = ['Guillaume Philippon <guillaume.philippon@lal.in2p3.fr>']
 
 from itopapi.model import ItopapiRack, ItopapiServer
-from itopapi.config import Config
+from itopapi.itopapiconfig import ItopapiConfig
