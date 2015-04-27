c33f23ec40b02e396f372ff764a69498ff48628f
Julien Nauroy <julien.nauroy@gmail.com>
2015-04-27 14:37:42 +0200

    Moved reading the configuration from a file to itopapiconfig

--- itopapi/itopapiconfig.py
+++ itopapi/itopapiconfig.py
@@ -7,6 +7,7 @@ ItopapiConfig holds the configuration for the whole project
 __version__ = '1.0'
 __authors__ = ['Julien Nauroy <julien.nauroy@u-psud.fr>']
 
+import ConfigParser
 
 class ItopapiConfig(object):
     """
@@ -21,3 +22,48 @@ class ItopapiConfig(object):
     api_version = '1.0'
     api_suffix = '/webservices/rest.php'
     quattor_profile = None
+    simulate_deletes = False
+
+    def read_config(self, config_file):
+        """
+        Read the configuration file given the file name given as an argument
+        and update the configuration accordingly
+        :param options:
+        :return:
+        """
+        config_parser = ConfigParser.ConfigParser()
+        config_parser.read(config_file)
+        try:
+            ItopapiConfig.hostname = config_parser.get('main', 'hostname')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            ItopapiConfig.username = config_parser.get('main', 'username')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            ItopapiConfig.password = config_parser.get('main', 'password')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            ItopapiConfig.protocol = config_parser.get('main', 'protocol')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            ItopapiConfig.base_suffix = config_parser.get('main', 'base_suffix')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            ItopapiConfig.api_version = config_parser.get('main', 'api_version')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            ItopapiConfig.api_suffix = config_parser.get('main', 'api_suffix')
+        except ConfigParser.NoOptionError:
+            pass
+        try:
+            simulate_deletes = config_parser.get('main', 'simulate_deletes')
+            if simulate_deletes.lower() == "true":
+                ItopapiConfig.simulate_deletes = simulate_deletes
+        except ConfigParser.NoOptionError:
+            pass
