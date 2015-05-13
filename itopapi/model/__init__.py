5e2f3c09f1f66944d0891122b7e57eaef8b97169
Julien Nauroy <julien.nauroy@gmail.com>
2015-05-13 12:05:39 +0200

    added partial list of missing classes to be processed later

--- itopapi/model/__init__.py
+++ itopapi/model/__init__.py
@@ -26,3 +26,6 @@ from itopapi.model.enclosure import ItopapiEnclosure
 from itopapi.model.brand import ItopapiBrand
 from itopapi.model.model import ItopapiModel
 from itopapi.model.applicationSolution import ItopapiApplicationSolution
+
+# TODO partial list of missing classes, with no particular order: Peripheral, MobilePhone, Printer, PC, Phone, IPPhone,
+# Tablet, TapeLibrary, SANSwitchNAS, PDU, PowerSource, DatabaseSchema, OtherSoftware
